7d715b1923cfded0fb5a7cf11326907492a9d641
CodeItQuick <evanontario@hotmail.com>
2024-12-24 06:57:06 -0400

    get tests working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import inquirer from 'inquirer';
 
-const questions = [
-    {
-        type: 'input',
-        name: 'name',
-        message: "What's your name?",
-    },
+const menuOptions = [
+    "0. Exit",
+    "1. (Incoming Radio) Add New Cab Driver",
+    "2. (Incoming Radio) Remove Cab Driver",
+    "3. (Outgoing Radio) Send Cab Driver Ride Request",
+    "4. (Incoming Radio) Cab Notifies Passenger Picked Up",
+    "5. (Incoming Radio) Cab Notifies Passenger Dropped Off",
+    "6. (Incoming Call) Cancel Cab Driver Fare",
+    "7. (Incoming Call) Customer Request Ride"
 ];
 
-inquirer.prompt(questions).then(answers => {
-    console.log(`Hi ${answers.name}!`);
-});
+let prompt;
+
+while (prompt?.name !== '0') {
+    menuOptions.forEach(x => console.log(x));
+
+    prompt = await inquirer.prompt([{ message: "Enter a selection: ", type: "input", name: "name" }]);
+
+    console.log(`You selected ${prompt.name}`);
+}
+
