9171da6b76bcbac65446c474c73da033b896e09b
CodeItQuick <evanontario@hotmail.com>
2025-02-02 07:29:03 -0400

    fix tests and test that just one cab can serve multiple call requests

--- tests/end-to-end/end-to-end-tests.spec.ts
+++ tests/end-to-end/end-to-end-tests.spec.ts
@@ -18,14 +18,14 @@ describe("End-to-end tests", () => {
         await prisma.customers.deleteMany();
     })
     test("program can select option 1 then 0", async () => {
-        const printLnFn = testPrintLnObj
+        const printLnFn = { ...testPrintLnObj, messages: [] };
 
         await program(printLnFn, messageReader(["1", "0"]));
 
         expect(printLnFn.messages).toContain("Dispatch recorded cab was added.");
     })
     test("program can remove a cab", async () => {
-        const printLnFn = testPrintLnObj
+        const printLnFn = { ...testPrintLnObj, messages: [] };
 
         await program(printLnFn, messageReader(["1", "2", "0"]));
 
@@ -33,14 +33,14 @@ describe("End-to-end tests", () => {
         expect(printLnFn.messages).toContain("Dispatch recorded cab was removed.");
     })
     test("program reports if no cab removed", async () => {
-        const printLnFn = testPrintLnObj
+        const printLnFn = { ...testPrintLnObj, messages: [] };
 
         await program(printLnFn, messageReader(["2", "0"]));
 
         expect(printLnFn.messages).toContain("No available cabs");
     })
     test("program can take a customer call", async () => {
-        const printLnFn = testPrintLnObj
+        const printLnFn = { ...testPrintLnObj, messages: [] };
 
         await program(printLnFn, messageReader(["1", "3", "0"]));
 
@@ -48,7 +48,7 @@ describe("End-to-end tests", () => {
         expect(printLnFn.messages).toContain("Dispatch received customer call for a ride.");
     })
     test("program can assign a cab", async () => {
-        const printLnFn = testPrintLnObj
+        const printLnFn = { ...testPrintLnObj, messages: [] };
 
         await program(printLnFn, messageReader(["1", "3", "5", "0"]));
 
@@ -57,7 +57,7 @@ describe("End-to-end tests", () => {
         expect(printLnFn.messages).toContain("Dispatch recorded cab requested.");
     })
     test("program can pickup a customer", async () => {
-        const printLnFn = testPrintLnObj
+        const printLnFn = { ...testPrintLnObj, messages: [] };
 
         await program(printLnFn, messageReader(["1", "3", "5", "6", "0"]));
 
@@ -67,7 +67,7 @@ describe("End-to-end tests", () => {
         expect(printLnFn.messages).toContain("Dispatch recorded cab has picked up customer."); // TODO: change message
     })
     test("program can drop off a cab", async () => {
-        const printLnFn = testPrintLnObj
+        const printLnFn = { ...testPrintLnObj, messages: [] };
 
         await program(printLnFn, messageReader(["1", "3", "5", "6", "7", "0"]));
 
@@ -77,8 +77,16 @@ describe("End-to-end tests", () => {
         expect(printLnFn.messages).toContain("Dispatch recorded cab has picked up customer.");
         expect(printLnFn.messages).toContain("Dispatch recorded customer is dropped off.");
     })
+    test("program can drop off multiple customer calls with a single cab", async () => {
+        const printLnFn = { ...testPrintLnObj, messages: [] };
+
+        await program(printLnFn, messageReader(["1", "3", "3", "5", "6", "7", "5", "6", "7", "0"]));
+
+        expect(printLnFn.messages).toContain("Dispatch recorded cab was added.");
+        expect(printLnFn.messages.filter(x => x === "Dispatch recorded customer is dropped off.").length).toEqual(2);
+    })
     test("program can cancel a cab", async () => {
-        const printLnFn = testPrintLnObj
+        const printLnFn = { ...testPrintLnObj, messages: [] };
 
         await program(printLnFn, messageReader(["1", "3", "4", "0"]));
 
