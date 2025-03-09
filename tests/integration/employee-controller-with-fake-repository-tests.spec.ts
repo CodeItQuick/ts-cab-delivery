caa149fcaf261f77d889473f8b735d3d9eb40be1
CodeItQuick <evanontario@hotmail.com>
2025-03-09 11:07:11 -0300

    remove dead code

--- tests/integration/employee-controller-with-fake-repository-tests.spec.ts
+++ tests/integration/employee-controller-with-fake-repository-tests.spec.ts
@@ -75,14 +75,6 @@ describe("Employee Repository Integration tests", () => {
         expect(await employeeRepository.find(2)).toEqual(undefined);
         expect(await employeeRepository.find(3)).toBeTruthy();
     });
-    // test("can promote an existing employee", async () => {
-    //     const employeeWithId = await createEmployee();
-    //
-    //     const employee = await promoteEmployee(employeeWithId!.id, 20);
-    //
-    //     expect(employee.CurrentWage.toString()).toEqual("20");
-    //     expect(employee.EmployeeName).toEqual("Evan");
-    // });
     test("can list all current employees", async () => {
         const employeeRepository = testableEmployeeRepository();
         const employeeOne: Employee = { CurrentWage: 10, EmployeeName: "Evan" };
@@ -95,44 +87,4 @@ describe("Employee Repository Integration tests", () => {
         expect(employees.length).toEqual(2);
         expect(typeof employees).toEqual("object")
     });
-    // test("can list all current employees", async () => {
-    //     await createEmployee();
-    //     await createEmployee();
-    //     await createEmployee();
-    //
-    //     const employees = await employeeList();
-    //
-    //     expect(employees.length).toEqual(3);
-    // });
-    // test("can clock into the timesheet system", async () => {
-    //     const employeeWithId = await createEmployee();
-    //
-    //     const timesheetEntry =
-    //         await clockIn(employeeWithId.id, new Date("Sun Feb 02 2025 15:38:25 GMT-0400 (Atlantic Standard Time)"));
-    //
-    //     expect(timesheetEntry.ClockInTimeYear).toEqual("2025");
-    //     expect(timesheetEntry.ClockInTimeMonth).toEqual("February");
-    //     expect(timesheetEntry.ClockInTimeDay).toEqual(2);
-    //     expect(timesheetEntry.ClockInTimeHour).toEqual(15);
-    //     expect(timesheetEntry.ClockInTimeMinute).toEqual(38);
-    // });
-    // test("can clock out of the timesheet system", async () => {
-    //     const employeeWithId = await createEmployee();
-    //     await clockIn(employeeWithId.id, new Date("Sun Feb 02 2025 15:38:25 GMT-0400 (Atlantic Standard Time)"));
-    //
-    //     const timesheetEntry =
-    //         await clockOut(employeeWithId.id,
-    //             new Date("Sun Feb 02 2025 16:38:25 GMT-0400 (Atlantic Standard Time)"));
-    //
-    //     expect(timesheetEntry.ClockInTimeYear).toEqual("2025");
-    //     expect(timesheetEntry.ClockInTimeMonth).toEqual("February");
-    //     expect(timesheetEntry.ClockInTimeDay).toEqual(2);
-    //     expect(timesheetEntry.ClockInTimeHour).toEqual(15);
-    //     expect(timesheetEntry.ClockInTimeMinute).toEqual(38);
-    //     expect(timesheetEntry.ClockOutTimeYear).toEqual("2025");
-    //     expect(timesheetEntry.ClockOutTimeMonth).toEqual("February");
-    //     expect(timesheetEntry.ClockOutTimeDay).toEqual(2);
-    //     expect(timesheetEntry.ClockOutTimeHour).toEqual(16);
-    //     expect(timesheetEntry.ClockOutTimeMinute).toEqual(38);
-    // });
 });
